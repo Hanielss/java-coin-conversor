5ecdf2585d23a5bb2a19a6af84987d1cf7e60210
hanielss <haniellemes@hotmail.com>
2023-09-04 20:11:04 -0300

    colocando-JavaScript-para-rodar-na-aplicação

---
+++ scripts.js
@@ -0,0 +1,16 @@
+const convertButton = document.querySelector(".convert-button")
+
+function convertValues (){
+
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+
+    const dolartoday = 5.2
+
+    const convertedValue = inputCurrencyValue / dolartoday
+
+    console.log(convertedValue)
+    
+}
+convertButton.addEventListener("click", convertValues)
+
+
